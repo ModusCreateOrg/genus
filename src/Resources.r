7f305ab14e0593e921304526cd3e8e2d86b68b99
Jay Garcia <jesusdgarcia@gmail.com>
2018-09-28 20:59:48 -0400

    Adding basic music

--- src/Resources.r
+++ src/Resources.r
@@ -8,3 +8,23 @@ PATH ../resources # path to resource files
 BITMAP bkg3.bmp
 BITMAP playernew.bmp
 
+# .-=[  MUSIC  ]=-.
+PATH ../resources/Music
+
+RAW Stage_2.xm
+RAW Stage_3.xm
+RAW Stage_4.xm
+RAW Stage_5.xm
+
+# .-=[ Sound Effects ]=-.
+PATH ../resources/sound_effects
+
+RAW SFX_boss_explode.wav
+RAW SFX_enemy_explode.wav
+RAW SFX_enemy_flyby.wav
+RAW SFX_enemy_shoot.wav
+RAW SFX_next_attract_char.wav
+RAW SFX_next_attract_screen.wav
+RAW SFX_player_hit.wav
+RAW SFX_player_shoot.wav
+RAW SFX_speed_boost.wav
