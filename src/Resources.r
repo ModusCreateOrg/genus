1bb978b6e32f14f9e6963ce9cc9e020c773db51e
Michael Tintiuc <contact@michaeltintiuc.com>
2018-11-28 19:15:18 +0200

    GEN-338

--- src/Resources.r
+++ src/Resources.r
@@ -6,13 +6,10 @@ BITMAP splash_sprites.bmp
 BITMAP menu_background.bmp
 BITMAP genus_logo.bmp
 BITMAP credits1.bmp
-BITMAP game_over1.bmp
 BITMAP game_over_sprites1.bmp
 BITMAP high_scores1.bmp
 BITMAP high_scores_animation1.bmp
-BITMAP main_menu1.bmp
 BITMAP main_options1.bmp
-BITMAP main_options_reset1.bmp
 
 BITMAP charset_8x8.bmp
 BITMAP charset_16x16.bmp
