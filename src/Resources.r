6832004de81b21638b5442911da35cccc2cda883
Jay Garcia <jesusdgarcia@gmail.com>
2019-02-24 12:17:42 -0500

    Reverting changes to files because they caused the segfault

--- src/Resources.r
+++ src/Resources.r
@@ -1,72 +1,85 @@
-####### Resource source file ########
+# Resource source file
+PATH ../resources # path to resource files
 
-## Common Assets
-PATH ../resources/common
-BITMAP game_over_sprites1.bmp
-BITMAP charset_8x8.bmp
-BITMAP charset_16x16.bmp
-BITMAP common_sprites.bmp
-RAW EmptySong.xm
-RAW GameOver.xm
-
-## Main Screens
-PATH ../resources/main_screens # path to resource files
 BITMAP splash1.bmp
 BITMAP splash_sprites.bmp
 BITMAP menu_background.bmp
 BITMAP genus_logo.bmp
+
 BITMAP modus_labs_sprite.bmp
 BITMAP team_sprite.bmp
-BITMAP main_options1.bmp
-RAW Main_Menu.xm
-RAW EnterCredits.xm
 
-## High Scores
-PATH ../resources/high_scores
+BITMAP game_over_sprites1.bmp
 BITMAP high_scores1.bmp
 BITMAP high_scores_animation1.bmp
+BITMAP main_options1.bmp
 
-
-## Stage 1 :: Countryside
-PATH ../resources/stage_1_countryside # path to resource files
+BITMAP charset_8x8.bmp
+BITMAP charset_16x16.bmp
+BITMAP common_sprites.bmp
 BITMAP stage1_sprites.bmp
-BITMAP countryside_static.bmp
-RAW Countryside.xm
-
-## Stage 2 :: UnderWaterOne
-PATH ../resources/stage_2_underwater_one # path to resource files
 BITMAP stage2_sprites.bmp
-BITMAP under_water_one.bmp
-RAW Under_Water.xm
-
-## Stage 3 :: Glacial mountains
-PATH ../resources/stage_3_glacial_mountains # path to resource files
 BITMAP stage3_sprites.bmp
-BITMAP glacial_mountains_static.bmp
-RAW Glacial_mountains.xm
-
-## Stage 4 :: Under Water Fantasy
-PATH ../resources/stage_4_underwater_fantasy # path to resource files
 BITMAP stage4_sprites.bmp
-BITMAP under_water_fantasy.bmp
-RAW UnderWaterFantasy.xm
-
-## Stage 5 :: Cyberpunk
-PATH ../resources/stage_5_cyberpunk # path to resource files
 BITMAP stage5_sprites.bmp
-BITMAP cyberpunk_static.bmp
-RAW Cyberpunk.xm
-
-## Stage 6 :: Spaaace
-PATH ../resources/stage_6_spaaace # path to resource files
 BITMAP stage6_sprites.bmp
+
+
+PATH ../resources/backgrounds/under_water
+BITMAP under_water_one.bmp
+
+PATH ../resources/backgrounds/countryside/
+#BITMAP countryside0.bmpadd 
+#BITMAP countryside1.bmp
+#BITMAP countryside2.bmp
+#BITMAP countryside3.bmp
+#BITMAP countryside4.bmp
+#BITMAP countryside5.bmp
+BITMAP countryside_static.bmp
+
+PATH ../resources/backgrounds/cyberpunk/
+#BITMAP cyberpunk0.bmp
+#BITMAP cyberpunk1.bmp
+#BITMAP cyberpunk2.bmp
+BITMAP cyberpunk_static.bmp
+
+PATH ../resources/backgrounds/glacial_mountains/
+#BITMAP glacial_mountains0.bmp
+#BITMAP glacial_mountains1.bmp
+#BITMAP glacial_mountains2.bmp
+#BITMAP glacial_mountains3.bmp
+#BITMAP glacial_mountains4.bmp
+#BITMAP glacial_mountains5.bmp
+BITMAP glacial_mountains_static.bmp
+
+PATH ../resources/backgrounds/under_water_fantasy/
+BITMAP under_water_fantasy.bmp
+#BITMAP under_water_fantasy0.bmp
+#BITMAP under_water_fantasy1.bmp
+#BITMAP under_water_fantasy2.bmp
+
+PATH ../resources/backgrounds/space/
+#BITMAP space_static.bmp
+#BITMAP space_static2.bmp
 BITMAP space_static_top_left.bmp
 BITMAP space_static_bottom_right.bmp
+
+# .-=[  MUSIC  ]=-.
+PATH ../resources/music
+RAW EmptySong.xm
+RAW Countryside.xm
+RAW Cyberpunk.xm
+RAW GameOver.xm
+RAW Glacial_mountains.xm
+RAW Main_Menu.xm
 RAW Spaaace.xm
+RAW UnderWaterFantasy.xm
+RAW Under_Water.xm
+RAW EnterCredits.xm
 
 
-## Sound Effects
-PATH ../resources/common/sound_effects
+# .-=[ Sound Effects ]=-.
+PATH ../resources/sound_effects
 
 RAW SFX_rotate_block_right.wav
 RAW SFX_rotate_block_left.wav
