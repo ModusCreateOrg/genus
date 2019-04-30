1f90f575456cff5b461fc4e5d0d04873131a15b0
Jay Garcia <jesusdgarcia@gmail.com>
2019-04-30 10:08:49 -0400

    Allow use of new CE Display classes

--- src/GameState/component.mk
+++ src/GameState/component.mk
@@ -1,2 +1,6 @@
 COMPONENT_PRIV_INCLUDEDIRS=. .. ${PROJECT_PATH}/src/GameState/Powerups ${PROJECT_PATH}/src/GameState/Playfields
-COMPONENT_EXTRA_INCLUDES=${CREATIVE_ENGINE_PATH}/src ${CREATIVE_ENGINE_PATH}/src/Widgets ${PROJECT_PATH}/src/GameState/Powerups ${PROJECT_PATH}/src/GameState/Playfields
+COMPONENT_EXTRA_INCLUDES=${CREATIVE_ENGINE_PATH}/src \
+    ${CREATIVE_ENGINE_PATH}/src/Display \
+    ${CREATIVE_ENGINE_PATH}/src/Widgets \
+    ${PROJECT_PATH}/src/GameState/Powerups \
+    ${PROJECT_PATH}/src/GameState/Playfields
