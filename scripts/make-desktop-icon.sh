f275aa4aa1a367e629bbb0302b3bac38edb8092c
Jay Garcia <jesusdgarcia@gmail.com>
2019-05-30 13:48:15 -0400

    New Mac and linux icon with a helper script

--- scripts/make-desktop-icon.sh
+++ scripts/make-desktop-icon.sh
@@ -1,38 +1,73 @@
 #!/usr/bin/env bash
 
-outputFolder="resources"
-sourcePNG="${outputFolder}/desktop-icon/GenusIcon.png"
-tempFolder="Genus.iconset"
+# this scripts assumes you have png2ico, sips and iconutil (mac) installed!
 
-mkdir -p ${tempFolder}
 
-function write_scaled_image() {
+
+outputDir="resources/desktop-icon"
+sourcePNG="${outputDir}/GenusIcon.png"
+tempDir="Genus.iconset"
+
+rm -rf ${tempDir} 2>/dev/null
+
+
+### Mac specific
+function scale_image_mac() {
 
   if [ -z ${2} ]; then
-    outFile="${tempFolder}/icon_${1}x${1}.png"
+    outFile="${tempDir}/icon_${1}x${1}.png"
   else
-    outFile="${tempFolder}/icon_${2}.png"
+    outFile="${tempDir}/icon_${2}.png"
   fi
 
   sips -z $1 $1 "${sourcePNG}" --out ${outFile} >/dev/null
 }
 
-write_scaled_image 16
-write_scaled_image 32 "16x16@2x"
-write_scaled_image 32 
-write_scaled_image 64 "32x32@2x"
-write_scaled_image 64 
-write_scaled_image 128 "64x64@2x"
-write_scaled_image 128
-write_scaled_image 256 "128x128@2x"
-write_scaled_image 256 
-write_scaled_image 512 "256x256@2x"
-write_scaled_image 512
-write_scaled_image 1024 "512x512@2x"
+mkdir -p ${tempDir}
 
-iconutil -c icns "${tempFolder}"
-mv Genus.icns ${outputFolder}/desktop-icon/
 
-echo "Created ${outputFolder}/desktop-icon/Genus.icns"
+scale_image_mac 16
+scale_image_mac 32 "16x16@2x"
+scale_image_mac 32 
+scale_image_mac 64 "32x32@2x"
+scale_image_mac 64 
+scale_image_mac 128 "64x64@2x"
+scale_image_mac 128
+scale_image_mac 256 "128x128@2x"
+scale_image_mac 256 
+scale_image_mac 512 "256x256@2x"
+scale_image_mac 512
+scale_image_mac 1024 "512x512@2x"
 
-rm -R ${tempFolder}
+iconutil -c icns "${tempDir}"
+mv Genus.icns ${outputDir}
+
+echo "Created mac icon at ${outputDir}/Genus.icns"
+
+rm -R ${tempDir}
+
+
+
+# Linux Specific
+
+mkdir ${tempDir}
+
+function scale_image_linux() {
+  outFile="${tempDir}/icon_${1}x${1}.png"
+  sips -z $1 $1 "${sourcePNG}" --out ${outFile} >/dev/null
+}
+
+scale_image_linux 16
+scale_image_linux 24
+scale_image_linux 32
+scale_image_linux 48
+scale_image_linux 64
+scale_image_linux 96
+scale_image_linux 128
+
+
+png2ico ${outputDir}/Genus.ico ${tempDir}/*.png >/dev/null
+echo "Created Linux icon at ${outputDir}/Genus.ico"
+
+rm -R ${tempDir}
+
