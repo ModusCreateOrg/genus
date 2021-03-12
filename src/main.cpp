a2fe9b0d6e5dbc461bd2b835d0c06f0762c55e97
Jay Garcia <jesusdgarcia@gmail.com>
2021-03-12 15:52:17 -0500

    Enabled network display

--- src/main.cpp
+++ src/main.cpp
@@ -6,20 +6,40 @@ BGameEngine *gGameEngine;
 GGame    *gGame;
 TOptions *gOptions;
 
+#ifdef __MODUS_TARGET_NETWORK_DISPLAY__
+#include "NetworkDisplay/NetworkDisplay.h"
+#endif
+
 /** @file
  * \brief Main entry point for game engine.
  *
  * Initializes the engine and runs the main event loop.
  */
-extern "C" void app_main() {
-  gGame = new GGame();
-  gGame->Run();
-  delete gGame;
+
+// app_main
+extern "C" void app_main(char *fileName) {
+
+    gGame = new GGame();
+#ifdef __MODUS_TARGET_NETWORK_DISPLAY__
+    NetworkDisplay *display = (NetworkDisplay*)&gDisplay;
+//    const char *fileName = "/home/jgarcia/modite-rgb.ini";
+    display->ConfigureFromIniFile(fileName);
+#endif
+    gGame->Run();
+    delete gGame;
 }
 
 
-int main() {
-  app_main();
-  return 0;
+int main(int argc, char *argv[]) {
+#ifdef __MODUS_TARGET_NETWORK_DISPLAY__
+  if (argc < 2) {
+    printf("Usage: %s <path.to.ini.file>\n", argv[0]);
+    return 1;
+  }
+#endif
+
+    app_main(argv[1]);
+    return 0;
 }
+
 
